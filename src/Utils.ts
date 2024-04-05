f94f0e3a3bf0d946ebe91d38b48373218bacf171
simps4simps <savarimani6@gmail.com>
2024-04-05 22:28:58 +0330

    Removed two functions: cause => numbering system not needed

--- src/Utils.ts
+++ src/Utils.ts
@@ -11,27 +11,8 @@ const getBrowserScreenData = (): IBrowserScreen => {
   return getBrowserScreenData;
 };
 
-const addToOpenTabs = (): void => {
-  const openedTabs: string | null = window.localStorage.getItem("TABSOPEN");
-  if (openedTabs) {
-    const openedTabsInt: number = parseInt(openedTabs) + 1;
-    window.localStorage.setItem("TABSOPEN", openedTabsInt.toString());
-  } else {
-    window.localStorage.setItem("TABSOPEN", "1");
-  }
+const addTabData = (tabData: string): void => {
+  localStorage.setItem("TAB_DATA", tabData);
 };
 
-const removeFromOpenTabs = (windowId: string): void => {
-  const openedTabs: string | null = window.localStorage.getItem("TABSOPEN");
-  if (openedTabs && openedTabs != "0") {
-    const openedTabsInt: number = parseInt(openedTabs) - 1;
-    window.localStorage.setItem("TABSOPEN", openedTabsInt.toString());
-    window.localStorage.setItem("TABCLOSED", windowId);
-  }
-};
-
-const addTabData = (tabId: number, tabData: string): void => {
-  localStorage.setItem(tabId.toString(), tabData);
-};
-
-export { getBrowserScreenData, addToOpenTabs, removeFromOpenTabs, addTabData };
+export { getBrowserScreenData, addTabData };
